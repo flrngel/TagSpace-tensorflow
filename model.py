b40149970a9c6fbbd753162b8b39580382d85440
flrngel <flrngel@gmail.com>
2017-09-19 23:44:31 +0900

    remove unnecessary comments, rename sexy variables

--- model.py
+++ model.py
@@ -9,7 +9,6 @@ class TagSpace(object):
     pass
 
   def create_model(self, l, tN, N=100000, d=10, K=5, H=1000, m=0.05, reuse=False):
-  #def create_model(self, l, tN, N=200000, d=100, K=5, H=1000, m=0.1, reuse=False):
     '''
     N = 1000000 (Paper)
     d = Unknown
@@ -20,12 +19,6 @@ class TagSpace(object):
       tag_flag = tf.placeholder('float32', shape=[None, tN], name='tag_flag')
 
       doc_embed = tflearn.embedding(doc, input_dim=N, output_dim=d)
-      #with tf.device('/cpu:0'):
-      #  itn = tf.shape(tag_flag)[0]
-      #  it = tf.tile([x for x in range(tN)], [itn])
-      #  ii = tf.reshape(it, [tf.shape(tag_flag)[0], tN])
-      #self.lt_embed = lt_embed = tflearn.embedding(ii, input_dim=tN, output_dim=d)
-      #self.lt_embed = lt_embed = tflearn.embedding(tf.ones_like(tag_flag), input_dim=tN, output_dim=d)
       self.lt_embed = lt_embed = tf.Variable(tf.random_normal([tN, d], stddev=0.1))
 
       net = tflearn.conv_1d(doc_embed, K, H, activation='tanh')
@@ -45,8 +38,8 @@ class TagSpace(object):
 
       self.positive_logit = positive_logit = tf.reduce_sum(tf.multiply(logit_set, tf.multiply(tg, lt_embed)), axis=2)
       with tf.device('/cpu:0'):
-        self.sexy = sexy = tf.map_fn(lambda x: (tf.boolean_mask(x[0], x[1]), True), (positive_logit, tf.not_equal(positive_logit, zero_vector)))
-      positive = tf.reduce_min(sexy[0], axis=1)
+        self.f_positive = f_positive = tf.map_fn(lambda x: (tf.boolean_mask(x[0], x[1]), True), (positive_logit, tf.not_equal(positive_logit, zero_vector)))
+      positive = tf.reduce_min(f_positive[0], axis=1)
       self.positive = positive
 
       tag_flag_ex = tf.expand_dims(1-tag_flag, 2)
@@ -54,32 +47,23 @@ class TagSpace(object):
       negative_logit = tf.reduce_sum(tf.multiply(logit_set, tf.multiply(tg, lt_embed)), axis=2)
 
       with tf.device('/cpu:0'):
-        self.sexy2 = sexy2 = tf.map_fn(lambda x: (tf.boolean_mask(x[0], x[1]), True), (negative_logit, tf.not_equal(negative_logit, zero_vector)))
-      negative = tf.reduce_max(sexy2[0], axis=1)
-      self.negative = negative
-      #negative = tf.reduce_max(tf.reduce_mean(tf.matmul(logit_set, tf.transpose(tf.multiply(tg, lt_embed))), axis=1))
+        self.f_negative = f_negative = tf.map_fn(lambda x: (tf.boolean_mask(x[0], x[1]), True), (negative_logit, tf.not_equal(negative_logit, zero_vector)))
+      self.negative = negative = tf.reduce_max(f_negative[0], axis=1)
 
-      #self.f_loss = f_loss = tf.square(-tf.reduce_mean(positive - negative))
-      print(tf.expand_dims(m - positive + negative,1))
-      print(tf.expand_dims(tf.fill([tf.shape(doc)[0]], 10e7),1))
-      print(tf.reduce_min([tf.expand_dims(m - positive + negative,1), tf.expand_dims(tf.fill([tf.shape(doc)[0]], 10e7),1)], axis=0))
-      print(tf.reduce_max([tf.reduce_min([tf.expand_dims(m - positive + negative,1), tf.expand_dims(tf.fill([tf.shape(doc)[0]], 10e7),1)], axis=0), tf.zeros([tf.shape(doc)[0], 1])], axis=0))
       self.f_loss = f_loss = tf.reduce_mean(tf.reduce_max([tf.reduce_min([tf.expand_dims(m - positive + negative,1), tf.expand_dims(tf.fill([tf.shape(doc)[0]], 10e7),1)], axis=0), tf.zeros([tf.shape(doc)[0], 1])], axis=0))
-      #self.f_loss = f_loss = tf.losses.hinge_loss(tag_flag, self.tag_logit)
-      #self.f_loss = f_loss = -tf.reduce_mean(positive - negative)
 
       opt = tf.train.AdamOptimizer(learning_rate=lr[0])
       self.op = opt.minimize(f_loss)
 
   def train_opts(self):
-    return [self.op, self.f_loss, self.logit, self.lt_embed, self.sexy[0][0], self.sexy2[0][0]]
+    return [self.op, self.f_loss, self.logit, self.lt_embed, self.f_positive[0][0], self.f_negative[0][0]]
 
   def test_opts(self):
     return [self.tag_logit]
 
 word_pad_length = 60
 tag_size = 5
-#tokenizer = tflearn.data_utils.VocabularyProcessor(word_pad_length)
+
 TOKENIZER_RE = re.compile(r"[A-Z]{2,}(?![a-z])|[A-Z][a-z]+(?=[A-Z])|[\'\w\-]+", re.UNICODE)
 def token_parse(iterator):
   for value in iterator:
@@ -93,24 +77,6 @@ def string_parser(arr, fit):
   else:
     return list(tokenizer.fit_transform(arr))
 
-#def read_my_file_format(filename_queue, tag_size):
-#  reader = tf.TextLineReader()
-#  key, record_string = reader.read(filename_queue)
-#  label, _, example = tf.decode_csv(record_string, [[0], ['1'], ['1']])
-#  processed_example = string_parser(example)
-#  label = tf.one_hot(tf.assign_sub(label, 1), tag_size)
-#  return processed_example, label
-#
-#def input_pipeline(filenames, batch_size, tag_size, num_epochs=None):
-#  filename_queue = tf.train.string_input_producer(filenames, num_epochs=num_epochs, shuffle=True)
-#  example, label = read_my_file_format(filename_queue, tag_size)
-#  min_after_dequeue = 100
-#  capacity = min_after_dequeue + 3 * batch_size
-#  example_batch, label_batch = tf.train.shuffle_batch(
-#      [example, label], batch_size=batch_size, capacity=capacity,
-#      min_after_dequeue=min_after_dequeue)
-#  return example_batch, label_batch
-
 model = TagSpace()
 with tf.Session() as sess:
   #with tf.device('/cpu:0'):
@@ -120,7 +86,6 @@ with tf.Session() as sess:
 
   sess.run(tf.global_variables_initializer())
 
-  #batch_words, batch_tags = input_pipeline(['./data/ag_news_csv/train.csv'], 32, tag_size, num_epochs=10)
   words, tags = tflearn.data_utils.load_csv('./data/ag_news_csv/train.csv', target_column=0, columns_to_ignore=[1], has_header=False, categorical_labels=True, n_classes=tag_size)
 
   from sklearn.utils import shuffle
@@ -135,7 +100,6 @@ with tf.Session() as sess:
   step_print = int((total/batch_size) / 13)
   global_step = 0
   lr = 1e-2
-  #lr_decr = 0
   lr_decr = (lr - (1e-9))/num_epochs
 
   print('start training')
@@ -166,6 +130,6 @@ with tf.Session() as sess:
     arr = result[0]
     for j in range(len(batch_tags)):
       rs+=np.sum(np.argmax(arr[j]) == np.argmax(batch_tags[j]))
-  print(rs/total)
+  print(f'Test accuracy: {rs/total}')
 
   sess.close()
